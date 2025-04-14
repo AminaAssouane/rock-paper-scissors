85118815a787181c43e0a8e5a4dde3bf0cbd9336
Amina Assouane <minassouane@gmail.com>
2025-04-14 17:13:40 +0100

    Write getComputerChoice function

---
+++ scripts.js
@@ -0,0 +1,8 @@
+getComputerChoice(){
+    const number = Math.floor(Math.random() * 100);
+
+    if (number < 33) return "ROCK";
+    else if (number < 66) return "PAPER";
+    else return "SCISSORS";
+}
+
