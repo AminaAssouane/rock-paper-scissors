88630def8c61e26ae09f1f85f3671ac967ebe0f0
Amina <minassouane@gmail.com>
2025-05-19 11:00:44 +0100

    Add div and delete console logs

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ let computerScore = 0;
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+const result = document.querySelector("#result");
 
 function getComputerChoice() {
   const number = Math.floor(Math.random() * 100);
@@ -12,26 +13,25 @@ function getComputerChoice() {
   else return "SCISSORS";
 }
 
-function getHumanChoice() {
-  let choice = prompt("Enter rock, paper or scissors");
-  return choice;
-}
+rock.addEventListener("click", () => playRound("ROCK", getComputerChoice()));
+paper.addEventListener("click", () => playRound("PAPER", getComputerChoice()));
+scissors.addEventListener("click", () =>
+  playRound("SCISSORS", getComputerChoice())
+);
 
 function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toUpperCase();
-
   if (humanChoice === "ROCK") {
     switch (computerChoice) {
       case "ROCK":
-        console.log("It's a tie!");
+        result.textContent = "It's a tie";
         break;
       case "PAPER":
         computerScore += 1;
-        console.log("Oh no! You lose! Paper beats rock.");
+        result.textContent = "Oh no! You lose! Paper beats rock.";
         break;
       case "SCISSORS":
         humanScore += 1;
-        console.log("Yes! You won! Rock beats scissors.");
+        result.textContent = "Yes! You won! Rock beats scissors.";
         break;
       default:
         break;
@@ -41,15 +41,15 @@ function playRound(humanChoice, computerChoice) {
   if (humanChoice === "PAPER") {
     switch (computerChoice) {
       case "PAPER":
-        console.log("It's a tie!");
+        result.textContent = "It's a tie";
         break;
       case "ROCK":
         humanScore += 1;
-        console.log("Yes! You won! Paper beats rock.");
+        result.textContent = "Yes! You won! Paper beats rock.";
         break;
       case "SCISSORS":
         computerScore += 1;
-        console.log("Oh no! You lose! Scissors beats paper.");
+        result.textContent = "Oh no! You lose! Scissors beats paper.";
         break;
       default:
         break;
@@ -59,15 +59,15 @@ function playRound(humanChoice, computerChoice) {
   if (humanChoice === "SCISSORS") {
     switch (computerChoice) {
       case "SCISSORS":
-        console.log("It's a tie!");
+        result.textContent = "It's a tie";
         break;
       case "ROCK":
         computerScore += 1;
-        console.log("Oh no! You lose! Rock beats scissors.");
+        result.textContent = "Oh no! You lose! Rock beats scissors.";
         break;
       case "PAPER":
         humanScore += 1;
-        console.log("Yes! You won! Scissors beats paper.");
+        result.textContent = "Yes! You won! Scissors beats paper.";
         break;
       default:
         break;
@@ -75,15 +75,11 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-function playGame() {
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
+/*function playGame() {
   if (humanScore > computerScore) console.log("CONGRATULATIONS! YOU WON!");
   else console.log("You lost.");
   humanScore = 0;
   computerScore = 0;
 }
 
-playGame();
+playGame();*/
